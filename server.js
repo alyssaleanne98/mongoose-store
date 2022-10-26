a2c12a1198c3a09687ecb2665bcbbed28360b699
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-25 22:35:59 -0700

    set-up

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Dependencies 
 const express = require('express');
 const app = express()
+const mongoose = require('mongoose')
 
+
+// database config
+const DATABASE_URL = "mongodb+srv://sei:aABlr4nXR1vg1WPC@cluster0.tertld9.mongodb.net/favoritebooks?retryWrites=true&w=majority"
 // config 
-const port = 4000
+// const port = 4000
 
 //  Middleware
 
@@ -18,7 +22,25 @@ app.get('/books', (req, res) => {
     res.send(books)
 });
 
-//Listener
-app.listen(port, () => {
-	console.log(`listening on port ${port}`)
+// Pulls environment vars into server.js from .env
+require('dotenv').config()
+const PORT = process.env.PORT 
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Database connection logs 
+const db = mongoose.connection
+db.on("error", (err) => console.log(err.message))
+db.on("connected", () => console.log("mongo connected"))
+db.on("disconnected", () => console.log("mongo disconnected"))
+
+// create 
+app.post("/books", (req, res) => {
+    res.send("received")
 })
+
+//Listener
+app.listen(PORT, ()=> console.log(`You are listening to the smooth sounds of port ${PORT}...`))
